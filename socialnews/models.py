a20e6bcce4df728055c6e42287663a4bfc65fc6b
Meysam Shirdel~ <mshirdel@gmail.com>
2018-09-19 17:46:19 +0430

    add models and bootstrap assets

--- socialnews/models.py
+++ socialnews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Item(TimeStampedModel):
+    title = models.CharField(max_length=500)
+    url = models.URLField(max_length=2000, blank=True, null=True)
+    item_body_text = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class ItemComment(TimeStampedModel):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item_comment = models.TextField()
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.item_comment[:100]
--- socialnews/views.py
+++ socialnews/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Item
+
+
 def index(request):
-    return HttpResponse('ok')
+    return render(request, 'socialnews/index.html', {'items': Item.objects.all()})
